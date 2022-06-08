84de2238d67c754f86a500902c773ab7e2ae3989
Djambek Tlepbergenov <d.d.tlepbergenov@gmail.com>
2022-06-08 21:53:10 +0300

    почти работает

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from blog import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('add', views.add, name="add_new_project"),
@@ -8,4 +9,6 @@ urlpatterns = [
     #path('change_up',views.change_up, name='change_up'),
     #path('change_down',views.change_down, name='change_down'),
     path('change',views.change, name='change'),
-]
+    path("", views.home, name="homepage"),
+    path("view", views.home, name="viewer"),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -14,35 +14,7 @@ def delete(request):
         el.position -= 1
     object.delete()
     return HttpResponse(status=200)
-# def change_up(request):
-#     object = Project.objects.filter(position=int(request.POST['position']))[0]
-#     print(object)
-#     object2 = Project.objects.filter(position__gt=min(int(request.POST['position']), int(request.POST['new_position'])+1 ),
-#                         position__lte=max(int(request.POST['new_position'])+1, int(request.POST['position'])))
-#     print(object2)
-#     if int(request.POST['new_position'])+1 > int(request.POST['position']):
-#         co = -1
-#     else:
-#         co = 1
-#     print(co)
-#     for i in object2:
-#                    i.position += co
-#                    print(i.id, i.position)
-#                    i.save()
-#     object.position = int(request.POST['new_position'])+1
-#     object.save()
-#     return HttpResponse(status=200)
-# # clon1234 admin
-# def change_down(request):
-#    object = Project.objects.filter(position=int(request.POST['position']))[0]
-#    object2 = Project.objects.filter(position__lt=int(request.POST['position']), position__gte=int(request.POST['new_position']))
-#    print(object2)
-#    for i in object2:
-#                   i.position += 1
-#                   i.save()
-#    object.position = int(request.POST['new_position'])+1
-#    object.save()
-#    return HttpResponse(status=200)
+
 
 def change(request):
     if int(request.POST['position'])>=int(request.POST['new_position']):
@@ -73,3 +45,9 @@ def change(request):
         object.position = int(request.POST['new_position'])+1
         object.save()
         return HttpResponse(status=200)
+
+
+
+def home(request):
+    print(Project.objects.order_by("position"))
+    return render(request, "blog/site.html", {"projects": Project.objects.order_by("position")})
